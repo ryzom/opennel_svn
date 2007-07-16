a97a2b19ad30af174ad2f326eca52b87e4dc082f
acemtp <acemtp@gmail.com>
2007-07-16 10:31:34 +0000

    Added: instance() wrapper for people that don't want to use getInstance()
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@58 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/include/nel/misc/singleton.h
+++ nel/include/nel/misc/singleton.h
@@ -69,6 +69,9 @@ namespace NLMISC
 			return *Instance;
 		}
 
+		/// shorter version of getInstance()
+		static T &instance() { return getInstance(); }
+
 		static void releaseInstance()
 		{
 			if(Instance)
