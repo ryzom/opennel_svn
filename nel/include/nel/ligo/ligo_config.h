efee941fe3fb8f49227180302d3222c5b17a73c3
mattr <matt.raykowski@gmail.com>
2007-08-06 20:01:51 +0000

    UPDATE: Removed ambiguous class identifier from header.
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@69 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/include/nel/ligo/ligo_config.h
+++ nel/include/nel/ligo/ligo_config.h
@@ -128,10 +128,10 @@ public:
 	bool isPrimitiveLinked (const NLLIGO::IPrimitive &primitive);
 
 	// Return the next primitive linked to 'primitive', or NULL
-	const NLLIGO::IPrimitive *CLigoConfig::getLinkedPrimitive (const NLLIGO::IPrimitive &primitive) const;
+	const NLLIGO::IPrimitive *getLinkedPrimitive (const NLLIGO::IPrimitive &primitive) const;
 
 	// Return the previous primitive linked to 'primitive', or NULL
-	const NLLIGO::IPrimitive *CLigoConfig::getPreviousLinkedPrimitive (const NLLIGO::IPrimitive &primitive) const;
+	const NLLIGO::IPrimitive *getPreviousLinkedPrimitive (const NLLIGO::IPrimitive &primitive) const;
 
 	// Is the primitive deletable ?
 	bool isPrimitiveDeletable (const NLLIGO::IPrimitive &primitive);
