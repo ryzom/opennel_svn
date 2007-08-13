b8189643d28f956897533c8a20ff44f8fb193c9c
acemtp <acemtp@gmail.com>
2007-08-13 10:19:26 +0000

    FIXED: debug mode doesn't link on Windows CHANGED: use another font REMOVED: old font
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@86 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/samples/3d/font/main.cpp
+++ nel/samples/3d/font/main.cpp
@@ -24,18 +24,10 @@
  * MA 02111-1307, USA.
  */
 
-#include <stdlib.h>
-#include <string>
-#include <windows.h>
-#undef min
-#undef max
-
 #include "nel/misc/types_nl.h"
 
-// look at event example
 #include "nel/misc/event_emitter.h"
 #include "nel/misc/event_listener.h"
-
 #include "nel/misc/path.h"
 
 // look at 3dinit example
@@ -47,16 +39,14 @@
 #include "3d/text_context.h"
 #include "3d/driver_user.h"
 
+
 using namespace std;
 using namespace NL3D;
 using namespace NLMISC;
 
+
 int main (int argc, char **argv)
 {
-	nlinfo("start");
-
-	CPath::addSearchPath("d:/code/nel/src/3d/driver/direct3d");
-
 	// look at 3dinit example
 	CNELU::init (800, 600, CViewport(), 32, true, 0, false, false); 
 
@@ -72,7 +62,7 @@ int main (int argc, char **argv)
 
 	// The first param is the font name (could be ttf, pfb, fon, etc...). The
 	// second one is optional, it's the font kerning file
-	tc.setFontGenerator ("Cyberbit.ttf");
+	tc.setFontGenerator ("arlrdbd.ttf");
 
 	// create the first computed string.
 	// A computed string is a string with a format and it generates the string
@@ -80,32 +70,22 @@ int main (int argc, char **argv)
 	// Second is a pointer to a font generator. 3rd is the color of the font.
 	// 4th is the size of the font. 5th is a pointer to the video driver.
 	// 6th is the resulting computed string.
-	CComputedString cptedString1;
-	fontManager.computeString ("Nevrax", tc.getFontGenerator(), CRGBA(255,255,255), 70, CNELU::Driver, cptedString1);
+	CComputedString csRotation;
+	fontManager.computeString ("cs Rotation", tc.getFontGenerator(), CRGBA(255,255,255), 70, CNELU::Driver, csRotation);
 
-	CComputedString cptedString3d;
-	fontManager.computeString ("3d string!!!", tc.getFontGenerator(), CRGBA(255,127,0), 75, CNELU::Driver, cptedString3d);
+	CComputedString cs3d;
+	fontManager.computeString ("cs 3d", tc.getFontGenerator(), CRGBA(255,127,0), 75, CNELU::Driver, cs3d);
 
-	ucstring ucs;
-	ucs = 28000;
-	ucs += 28108;
-	ucs += 28188;
-	ucs += 28858;
-	// generate an Unicode string. You can see it only if you have an Unicode font.
-	// we haven't find a GPL Unicode font yet :(
-	CComputedString cptedString4;
-	fontManager.computeString (ucs, tc.getFontGenerator(), CRGBA(32,64,127), 75, CNELU::Driver, cptedString4);
+	// generate an Unicode string.
+	ucstring ucs("cs Unicode String");
 
-/*
-	CComputedString cptedString[10];
-	for (uint i = 0; i < 10; i++)
-		fontManager.computeString ("Nevrax", &fontGenerator, CRGBA(255,255,255), i*10, CNELU::Driver, cptedString[i]);
-*/
+	CComputedString csUnicode;
+	fontManager.computeString (ucs, tc.getFontGenerator(), CRGBA(32,64,127), 75, CNELU::Driver, csUnicode);
 
 	// look at event example
 	CNELU::EventServer.addEmitter(CNELU::Driver->getEventEmitter());
 	CNELU::AsyncListener.addToServer(CNELU::EventServer);
-	
+
 	do
 	{
 		// look at 3dinit example
@@ -124,7 +104,7 @@ int main (int argc, char **argv)
 		// display a string in 3d
 		// first param is a pointer to the driver. second one is
 		// the matrix transformation for the string
-		cptedString3d.render3D (*CNELU::Driver, m);
+		cs3d.render3D (*CNELU::Driver, m);
 
 		// the first param is a pointer to a driver. second one is the x position
 		// (between (0.0 (left) and 1.0 (right)). third one is the y position (between
@@ -132,7 +112,7 @@ int main (int argc, char **argv)
 		tc.setColor (CRGBA (255, 255, 255));
 		tc.setFontSize (40);
 		tc.setHotSpot (CComputedString::BottomLeft);
-		tc.printAt (0.5f, 0.7f, string("Nevrax"));
+		tc.printAt (0.5f, 0.7f, string("printAt"));
 
 		// the fourth param is the position of the hotspot, the text will be displayed at x,y
 		// depending on the hotspot
@@ -141,9 +121,9 @@ int main (int argc, char **argv)
 		tc.setHotSpot (CComputedString::BottomLeft);
 		tc.printAt (0.0f, 0.0f, string("NeL"));
 
-		static float scale=1.0f, way=0.01f;
+		static float scale=1.0f, way=0.05f;
 		scale+=way;
-		if (scale>5 || scale < 1) way = -way;
+		if (scale>4 || scale < 1) way = -way;
 		// the 5th and 6th params are the scale of the texture
 
 		tc.setColor (CRGBA (200, 255, 64));
@@ -151,35 +131,27 @@ int main (int argc, char **argv)
 		tc.setHotSpot (CComputedString::BottomLeft);
 		tc.setScaleX (scale);
 		tc.setScaleZ (scale);
-		tc.printAt (0.1f, 0.3f, string("Nevrax Library"));
+		tc.printAt (0.1f, 0.3f, string("printAt Scale String"));
 		
 		// display the same string with no scale
 		tc.setHotSpot (CComputedString::TopLeft);
 		tc.setScaleX (1.0f);
 		tc.setScaleZ (1.0f);
-		tc.printAt (0.1f, 0.2f, string("Nevrax Library"));
+		tc.printAt (0.1f, 0.25f, string("printAt NoScale String"));
 
 		// the 7th params is the rotation in radian
 		static float angle=0.0f;
 		angle+=0.01f;
-		cptedString1.render2D (*CNELU::Driver, 0.2f, 0.7f, CComputedString::MiddleMiddle, 1, 1, angle);
+		csRotation.render2D (*CNELU::Driver, 0.2f, 0.7f, CComputedString::MiddleMiddle, 1, 1, angle);
+
+		csUnicode.render2D (*CNELU::Driver, 1.0f, 0.15f, CComputedString::MiddleRight);
 
 		// display the Unicode string
 		tc.setColor (CRGBA (32, 64, 127));
-		tc.setFontSize (70);
+		tc.setFontSize (65);
 		tc.setHotSpot (CComputedString::MiddleRight);
-		tc.printAt (1.0f, 0.2f, ucs);
-/*
-		uint i;
-		for (i = 0; i < 10; i++)
-		{
-			fontManager.computeString ("Nevrax", &fontGenerator, CRGBA(255,255,255), i*10, CNELU::Driver, cptedString[i]);
-			cptedString[i].render2D (*CNELU::Driver, 0.0f, i/10.0f, CComputedString::MiddleLeft);
-		}
+		tc.printAt (1.0f, 0.85f, ucstring("printAt Unicode String"));
 
-		for (i = 0; i < 10; i++)
-			cptedString[i].render2D (*CNELU::Driver, 0.5f, i/10.0f, CComputedString::MiddleLeft);
-*/
 		tc.setColor (CRGBA (255, 127, 0));
 		tc.setFontSize (20);
 		tc.setHotSpot (CComputedString::BottomRight);
@@ -188,21 +160,13 @@ int main (int argc, char **argv)
 		// look 3dinit example
 		CNELU::swapBuffers();
 		CNELU::screenshot();
-		
+
 		// look at event example
 		CNELU::EventServer.pump(true);
-
-/*		MSG	msg;
-		while ( PeekMessage(&msg,NULL,0,0,PM_REMOVE) )
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-*/
 	}
 	while(!CNELU::AsyncListener.isKeyPushed(KeyESCAPE));
 
-	fontManager.dumpCache ("ok.tga");
+	fontManager.dumpCache ("font_cache_dump.tga");
 
 	// look at event example
 	CNELU::AsyncListener.removeFromServer(CNELU::EventServer);
