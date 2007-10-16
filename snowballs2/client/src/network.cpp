21aa6fc54844aaa42282d08990f2f374bb37b06a
mattraykowski <matt.raykowski@gmail.com>
2007-10-16 15:44:23 +0000

    UPDATE: Steps towards enabling online-mode.
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@199 bf685bcc-7839-4fcd-9254-36823579bede

--- snowballs2/client/src/network.cpp
+++ snowballs2/client/src/network.cpp
@@ -329,7 +329,7 @@ void	initNetwork(const std::string &lc, const std::string &addr)
 		loginCookie.setFromString (lc);
 	}
 
-	string res;// TODO = CLoginClient::connectToShard (loginCookie, fsaddr, *Connection);
+	string res = CLoginClient::connectToShard (loginCookie, fsaddr, *Connection);
 	if (!res.empty ())
 	{
 		string err = string ("Connection to shard failed: ") + res;
