f8588e38f288d55fa7ab01a098e2ce9cdec2806a
acemtp <acemtp@gmail.com>
2007-12-12 18:13:47 +0000

    ADDED: detection of libtoolize on mac
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@347 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/autogen.sh
+++ nel/autogen.sh
@@ -2,14 +2,25 @@
 
 WANT_AUTOMAKE="1.6"
 
+case `uname -s` in
+Darwin)
+	LIBTOOLIZE=glibtoolize
+	;;
+*)
+	LIBTOOLIZE=libtoolize
+	;;
+esac
+
 echo "Creating macros..." && \
 aclocal -I automacros/ && \
 echo "Creating library tools..." && \
-libtoolize --force && \
+$LIBTOOLIZE --force && \
 echo "Creating header templates..." && \
 autoheader && \
 echo "Creating Makefile templates..." && \
 automake --gnu --add-missing && \
 echo "Creating 'configure'..." && \
 autoconf && \
-echo -e "\nRun: ./configure; make; make install\n"
+echo "" && \
+echo "Run: ./configure; make; make install" && \
+echo ""
