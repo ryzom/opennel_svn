0fb7568877be4da5277a5f1bb40a08921e9cdfe2
acemtp <acemtp@gmail.com>
2007-12-12 18:00:44 +0000

    ADDED: special case for mac
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@338 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/src/misc/mutex.cpp
+++ nel/src/misc/mutex.cpp
@@ -332,11 +332,15 @@ namespace NLMISC {
 
 CUnfairMutex::CUnfairMutex()
 {
-	pthread_mutexattr_t attr;
+    pthread_mutexattr_t attr;
 	pthread_mutexattr_init( &attr );
 	// Fast mutex. Note: on Windows all mutexes are recursive
-	pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE_NP ); //PTHREAD_MUTEX_ERRORCHECK_NP );//PTHREAD_MUTEX_ADAPTIVE_NP );
-	pthread_mutex_init( &mutex, &attr );
+#ifdef NL_OS_MAC
+            pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE );
+#else
+            pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE_NP );
+#endif
+            pthread_mutex_init( &mutex, &attr );
 	pthread_mutexattr_destroy( &attr );
 }
 
@@ -349,8 +353,12 @@ CUnfairMutex::CUnfairMutex(const std::string &name)
 	pthread_mutexattr_t attr;
 	pthread_mutexattr_init( &attr );
 	// Fast mutex. Note: on Windows all mutexes are recursive
-	pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE_NP ); //PTHREAD_MUTEX_ERRORCHECK_NP );//PTHREAD_MUTEX_ADAPTIVE_NP );
-	pthread_mutex_init( &mutex, &attr );
+#ifdef NL_OS_MAC
+            pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE );
+#else
+            pthread_mutexattr_setkind_np( &attr, PTHREAD_MUTEX_RECURSIVE_NP );
+#endif
+           pthread_mutex_init( &mutex, &attr );
 	pthread_mutexattr_destroy( &attr );
 }
 
