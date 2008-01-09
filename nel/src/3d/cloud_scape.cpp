fd3eb7c1a24fed9eafc4cccd29ea5bd857127bf3
acemtp <acemtp@gmail.com>
2008-01-09 18:41:44 +0000

    CHANGED: do not use PerformanceCounter
    git-svn-id: svn://svn.code.sf.net/p/nel/code/trunk@480 bf685bcc-7839-4fcd-9254-36823579bede

--- nel/src/3d/cloud_scape.cpp
+++ nel/src/3d/cloud_scape.cpp
@@ -472,7 +472,7 @@ void CCloudScape::set (SCloudScapeSetup &css)
 // ------------------------------------------------------------------------------------------------
 void CCloudScape::anim (double dt, NL3D::CCamera *pCamera)
 {
-	double startDate = NLMISC::CTime::ticksToSecond(NLMISC::CTime::getPerformanceTime());
+	double startDate = CTime::getLocalTime();
 	sint32 i;
 
 	// Disable fog
@@ -626,8 +626,8 @@ void CCloudScape::anim (double dt, NL3D::CCamera *pCamera)
 			i++;
 		}
 	}
-	double endDate = NLMISC::CTime::ticksToSecond(NLMISC::CTime::getPerformanceTime());
-	_LastAnimRenderTime = endDate - startDate;	
+	double endDate = CTime::getLocalTime();
+	_LastAnimRenderTime = endDate - startDate;
 }
 
 // ------------------------------------------------------------------------------------------------
